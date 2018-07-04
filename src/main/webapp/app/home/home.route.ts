42890203eb2fc45a95f1071b1f98664f8c345e0d
gaokangkang <904451260@qq.com>
2018-07-04 23:01:23 +0800

    问题模块修改

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './';
 
 export const HOME_ROUTE: Route = {
     path: '',
-    component: HomeComponent,
-    data: {
-        authorities: [],
-        pageTitle: 'home.title'
-    }
+    // component: HomeComponent,
+    // data: {
+    //     authorities: [],
+    //     pageTitle: 'home.title'
+    // },
+    redirectTo: '/question-management/question-accredit?id=1', pathMatch: 'full'
 };
